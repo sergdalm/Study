5e1672762e5da1c729e27f30c4548334e0297316
Alex <30310397+sergdalm@users.noreply.github.com>
2022-01-25 08:42:20 +0300

    SQL practice
    SQL practice

---
+++ src/io1/ReaderRunner.java
@@ -0,0 +1,19 @@
+package io1;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.stream.Collectors;
+
+public class ReaderRunner {
+    public static void main(String[] args) throws IOException {
+        File file = Path.of("resources1", "text.txt").toFile();
+        try (BufferedReader fileReader = new BufferedReader(new FileReader(file))) {
+            String collect = fileReader.lines()
+                    .collect(Collectors.joining("\n"));
+            System.out.println(collect);
+        }
+    }
+}
