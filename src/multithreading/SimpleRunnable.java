dd6ca09402e53a3895ad8e9dc81467c3e76d5b55
Alex <30310397+sergdalm@users.noreply.github.com>
2022-02-01 11:56:47 +0300

    Multithreading practice
    Multithreading practice

---
+++ src/multithreading/SimpleRunnable.java
@@ -0,0 +1,8 @@
+package multithreading;
+
+public class SimpleRunnable implements Runnable{
+    @Override
+    public void run() {
+        System.out.println("Hello from runnable: " + Thread.currentThread().getName());
+    }
+}
---
+++ src/multithreading/ThreadDemo.java
@@ -0,0 +1,22 @@
+package multithreading;
+
+public class ThreadDemo {
+    public static void main(String[] args) {
+        var simpleThread = new SimpleThread();
+        var runnableThread = new Thread(new SimpleRunnable(), "My Name");
+        var lambdaThread = new Thread(() -> System.out.println("Hello from lambda: " + Thread.currentThread().getName()));
+
+        simpleThread.start();
+        runnableThread.start();
+        lambdaThread.start();
+
+        try {
+            simpleThread.join();
+            runnableThread.join();
+            lambdaThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(Thread.currentThread().getName());
+    }
+}
