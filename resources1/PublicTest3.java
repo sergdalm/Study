8ae58ad26caf8eeba2211f174e179bbc3ff31b47
sergdalm <30310397+sergdalm@users.noreply.github.com>
2022-02-01 20:13:45 +0300

    IO practice

---
+++ resources1/PublicTest3.java
@@ -0,0 +1,33 @@
+;ecitcarp.oi egakcap
+
+;noitpecxEOI.oi.avaj tropmi
+;seliF.elif.oin.avaj tropmi
+;htaP.elif.oin.avaj tropmi
+;rennacS.litu.avaj tropmi
+
+{ 3ksaT ssalc cilbup
+{ noitpecxEOI sworht )sgra ][gnirtS(niam diov citats cilbup    
+;)"txt.3ksat" ,"1secruoser"(fo.htaP = htap htaP        
+)(maerts.)htap(seniLllAdaer.seliF        
+)tnuoCstigiDxaMdnif::3ksaT(pam.                
+;)nltnirp::tuo.metsyS(hcaErof.                
+
+}    
+
+{ )enil gnirtS(tnuoCstigiDxaMdnif regetnI citats cilbup    
+;0 = tluser tni        
+;0 = retnuoc tni        
+{ )++i ;)(htgnel.enil < i ;0 = i tni( rof        
+{ )))i(tArahc.enil(tigiDsi.retcarahC(fi            
+;++retnuoc                
+{ esle }            
+)retnuoc < tluser(fi                
+;retnuoc = tluser                    
+;0 =retnuoc                
+}            
+)retnuoc < tluser(fi            
+;retnuoc = tluser                
+}        
+;tluser nruter        
+}    
+}
