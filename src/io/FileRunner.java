8a0d165d2bfa8512508537bb779ebefdab1aee0b
Alex <30310397+sergdalm@users.noreply.github.com>
2022-02-01 11:17:01 +0300

    IO File class
    IO File class practice

--- src/io/FileRunner.java
+++ src/io/FileRunner.java
@@ -18,10 +18,20 @@ public class FileRunner {
         System.out.println("canRead: " + file.canRead());
         System.out.println("canWrite: " + file.canWrite());
         System.out.println(file.getAbsolutePath());
+        System.out.println("absolute: " + file.isAbsolute());
+        System.out.println("file size: " + file.length() + " bytes");
+        long fileSize = file.length();
+        long fileFreeSpace = file.getFreeSpace();
+        long totalSpace = file.getTotalSpace();
+        System.out.println("free space: " + file.getFreeSpace());
+        System.out.println("Total file space: " + (fileSize + fileFreeSpace));
+        System.out.println("getTotalSpace: " + file.getTotalSpace());
+        System.out.println("difference: " + (totalSpace - (fileSize + fileFreeSpace)));
+        System.out.println("usable space: " + file.getUsableSpace());
+        System.out.println("last modified: " + file.lastModified());
 
         File dir = new File("resources/test/dir");
         dir.mkdirs();
         System.out.println("dir.list: " + Arrays.toString(dir.list()));
-
     }
 }
